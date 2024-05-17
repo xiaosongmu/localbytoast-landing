0a71788ae3317b2fcafa1e9e401d85ff973d2714
Noah Bernsohn <bernsno@gmail.com>
2024-05-17 09:15:30 -0500

    add images

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        surface: '#F8F5F2',
+      },
+    },
+  },
+  plugins: [],
+};
