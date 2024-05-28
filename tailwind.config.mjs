07778cc96bf4484718b23930b2034734b5944813
noahbernsohn-toast <86251098+noahbernsohn-toast@users.noreply.github.com>
2024-05-28 15:41:55 -0500

    more updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,41 +1,20 @@
+import baseConfig from "@toasttab/buffet-pui-styles/consumer"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/@toasttab/**/dist/**/*.@(js|jsx|ts|tsx|css)'
   ],
   theme: {
+    ...baseConfig.theme,
     extend: {
+      ...baseConfig.theme.extend,
       colors: {
         surface: '#F8F5F2',
       },
-      fontFamily: {
-        sans: [
-          'Source-Sans-Pro',
-          'system-ui',
-          '-apple-system',
-          '"Helvetica Neue"',
-          'Arial'
-        ],
-        effra: [
-          'Effra',
-          'system-ui',
-          '-apple-system',
-          '"Helvetica Neue"',
-          'Arial'
-        ],
-      animation: {
-        'loop-scroll-slow': 'loop-scroll 400s linear infinite',
-        'loop-scroll-med': 'loop-scroll 300s linear infinite',
-        'loop-scroll-fast': 'loop-scroll 150s linear infinite',
-      },
-      keyframes: {
-        'loop-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100vw)' },
-        },
-      },
     },
+    
   },
-  plugins: [],
-  }, 
-}
+  plugins: [...baseConfig.plugins]
+};
