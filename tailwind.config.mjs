cb4895b263edb3a2a59cd36e8eeaa6bf8fdb4127
noahbernsohn-toast <86251098+noahbernsohn-toast@users.noreply.github.com>
2024-07-11 16:13:59 -0500

    remove buffet and fix js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,30 @@
-import baseConfig from "@toasttab/buffet-pui-styles/consumer"
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './node_modules/@toasttab/**/dist/**/*.@(js|jsx|ts|tsx|css)'
   ],
   theme: {
-    ...baseConfig.theme,
     extend: {
-      ...baseConfig.theme.extend,
-      colors: {
-        surface: '#F8F5F2',
+      fontFamily: {
+        sans: [
+          'Source-Sans-Pro',
+          'system-ui',
+          '-apple-system',
+          '"Helvetica Neue"',
+          'Arial'
+        ],
+        effra: [
+          'Effra',
+          'system-ui',
+          '-apple-system',
+          '"Helvetica Neue"',
+          'Arial'
+        ],
+        mono: ['Menlo', 'Monaco', 'Consolas']
       },
+
     },
     
   },
-  plugins: [...baseConfig.plugins]
+  plugins: []
 };
