18a06eca88b153fdda11b2c61ef34ee3592bd814
noahbernsohn-toast <86251098+noahbernsohn-toast@users.noreply.github.com>
2024-07-11 18:32:50 -0500

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://xiaosongmu.github.io/'
+  site: 'https://xiaosongmu.github.io/localbytoast-landing/'
 });
