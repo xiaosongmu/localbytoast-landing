14306eb7990e5a818f26ed2f2dd6ebaae15df3e9
XiaoSong <xiaosong@xiaosongs-mbp.lan>
2024-07-18 17:08:03 -0500

    revert changes to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
